6bae94192292ec0cb6a3cc5b4b9b90d7c79fd446
Sudok <gsr@videvince.com>
2020-08-26 21:59:04 -0300

    Creating statur for game, and adding log

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,5 +1,6 @@
 defmodule ExMon do
   alias ExMon.{Game, Player}
+  alias ExMon.Game.Status
 
   @computer_name "Robotinik"
 
@@ -11,5 +12,9 @@ defmodule ExMon do
     @computer_name
     |> create_player(:punch, :kick, :heal)
     |> Game.start(player)
+
+    Status.print_round_message()
   end
 end
+
+# player = ExMon.create_player("Glauber", :soco, :chute, :cura)
